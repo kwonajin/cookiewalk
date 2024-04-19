a4d7ddf3e07c1b2de973a8a7bfcf223c5d322297
Jistan7011 <qus7011@gmail.com>
2024-04-19 09:52:24 +0900

    for pull

--- cookiewalk/src/App.jsx
+++ cookiewalk/src/App.jsx
@@ -1,15 +1,35 @@
-// import Home from "./main/home"
-import MyPage from "./mypage/mypage"
+import Home from "./main/home"
+// import MyPage from "./mypage/mypage"
 // import Pause from "./startpage/pause";
-// import LogIn from "./login/login"
+import LogIn from "./login/login"
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
-function App(){
-  return(
-    <>
-      <MyPage/>
-    </>
-  )
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+
+export default App;
+
+
+// function App(){
+//   return(
+//     <>
+//       <LogIn/>
+//     </>
+//   )
+// }
+
+// export default App
+
