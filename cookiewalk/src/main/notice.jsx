212b0ec26942b0b3823005490d3fdba8f1c75766
kwonajin <kaj0529@naver.com>
2024-04-26 09:42:36 +0900

    notice page 완성

--- cookiewalk/src/main/notice.jsx
+++ cookiewalk/src/main/notice.jsx
@@ -12,49 +12,40 @@ export default function Notice() {
         <div className="notice_line1"></div>
       </div>
 
-      <div className="e158_220">
-        <div className="e158_215">
-          <div className="e158_216"></div>
-          <div className="e158_217"></div>
-          <span className="e158_218">good_running_day님이 회원의 게시물을 좋아합니다.</span>
-          <div className="e158_219"></div>
-        </div>
-        <div className="e155_36"></div>
+      <div className="notice1">
+          <img className='notice1_profile' src="./images/ellipse_11.png" alt="" />
+          <div className="notice1_text">good_running_day님이 회원의 게시물을 좋아합니다.</div>
+          <img className="notice1_content_img" src="./images/rectangle_2.png" alt="" />
+        <div className="notice1_line"></div>
       </div>
-      <div className="e158_221">
-        <div className="e158_223"></div>
-        <div className="e158_229">
-          <div className="e158_231"></div>
-          <span className="e158_232">good_running_day님이 회원의 게시물에 댓글을 남겼습니다.</span>
-          <div className="e158_233"></div>
-        </div>
-        <div className="e158_234"></div>
+      
+      <div className="notice2">
+        <img className='notice2_profile' src="./images/ellipse_11.png" alt="" />
+          <div className="notice2_text">good_running_day님이 회원의 게시물에 댓글을 남겼습니다.</div>
+          <img className="notice2_content_img" src="./images/rectangle_2.png" alt="" />
+        <div className="notice2_line"></div>
       </div>
-      <div className="e158_235">
-        <div className="e158_243">
-          <div className="e158_244"></div>
-          <div className="e158_245"></div>
+
+
+      <div className="notice3">
+          <img className="notice3_profile" src="./images/ellipse_11.png" alt="" />
+          <div className="notice3_text">good_running_day님이 회원님을 팔로우하기 시작했습니다.</div>
+        <div className="notice3_following">
+          <div className="notice3_following_text">팔로잉</div>
         </div>
-        <div className="e158_248"></div>
-        <div className="e158_267">
-          <div className="e155_47"></div>
-          <span className="e155_50">팔로잉</span>
-        </div>
+        <div className="notice3_line"></div>
       </div>
-      <div className="e158_249">
-        <div className="e158_257">
-          <div className="e158_258"></div>
-          <div className="e158_259"></div>
+
+      <div className="notice4">
+      <img className='notice4_profile' src="./images/ellipse_11.png" alt="" />
+          <div className="notice4_text">good_running_day님이 회원님을 팔로우하기 시작했습니다.</div>
+        <div className="notice4_following">
+          <div className="notice4_following_text">팔로우</div>
         </div>
-        <div className="e158_262"></div>
-        <div className="e158_266">
-          <div className="e155_54"></div>
-          <span className="e155_55">팔로우</span>
-        </div>
-        <div className="e158_293"><span className="e158_295">팔로우</span></div>
+        <div className="notice4_line"></div>
       </div>
-      <span className="e155_53">good_running_day님이 회원님을 팔로우하기 시작했습니다.</span>
-      <span className="e155_46">good_running_day님이 회원님을 팔로우하기 시작했습니다.</span>
+
+
     </div>
   );
 };
