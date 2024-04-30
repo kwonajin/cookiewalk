36c6a37811689a7c516e57c87815d436f01f57c3
dh0728 <dustkscjswo@naver.com>
2024-04-30 14:55:10 +0900

    중간저장

--- cookiewalk/src/App.jsx
+++ cookiewalk/src/App.jsx
@@ -28,22 +28,7 @@ export default function App() {
     async function checkToken() {
       const {data,error}= await supabase.auth.getUser();
       setUser(data);
-      console.log(user,1111);
-      const authString = localStorage.getItem('sb-rbdndushdupsmytidea-auth-token');
-      console.log(authString)
-      if (authString) {
-        try {
-          const authObject = JSON.parse(authString);
-          const accessToken = authObject.access_token;
-          console.log('Access Token:', accessToken);
-          return accessToken;
-        } catch (e) {
-          console.error('Error parsing auth token from local storage:', e);
-        }
-      } else {
-        console.log('No auth token found in local storage.');
-        return null;
-      }
+      console.log(user);
     }
     checkToken();
   }, []);
