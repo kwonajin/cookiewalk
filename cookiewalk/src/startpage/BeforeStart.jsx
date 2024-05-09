3d2d5f68a81076697f2a731a275e206f83be5e13
broisHyeong <limminhyeong@gmail.com>
2024-05-09 12:01:10 +0900

    mod map_draw_icon and start_close button

--- cookiewalk/src/startpage/BeforeStart.jsx
+++ cookiewalk/src/startpage/BeforeStart.jsx
@@ -81,7 +81,7 @@ export default function BeforeStart(){
 
     return(
         <div className="BeforeStart_container">
-            <div><img className='e118_437' src="./icon/arrow.svg"/></div>
+            <Link to='/home'><div><img className='e118_437' src="./icon/arrow.svg"/></div></Link>
             <div><img className='e118_439' src="./icon/setting.svg"/></div>
             
             <MapDiv className='MapStyle'><MyMap center={currentPosition} /></MapDiv>
