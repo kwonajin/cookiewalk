7ffd7f8098c32189693aba418f578f33fb2ee215
kwonajin <kaj0529@naver.com>
2024-05-13 12:37:53 +0900

    write dropdown create

--- cookiewalk/src/mypage/blocked.jsx
+++ cookiewalk/src/mypage/blocked.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './saved.css'
+import './blocked.css'
 
-export default function Saved() {
+export default function Blocked() {
   return (
     <div className="saved_background">
       <Link to="/mypage_menu"><div className="saved_back"><img className="saved_back_icon" src="./icon/arrow.svg" alt="" /></div></Link>
 
-      <span className="saved_title">저장한 게시물</span>
+      <span className="saved_title">차단한 사용자</span>
       <div className="saved_line"></div>
 
     </div>
