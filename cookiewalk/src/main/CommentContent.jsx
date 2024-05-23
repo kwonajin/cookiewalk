088f11cdaf4852f1e72c10cf0b3046c4a7864a14
broisHyeong <limminhyeong@gmail.com>
2024-05-23 09:35:57 +0900

    mod comment.css

--- cookiewalk/src/main/CommentContent.jsx
+++ cookiewalk/src/main/CommentContent.jsx
@@ -9,8 +9,8 @@ export default function CommentContent()
         <div className="comment-username">good_running_day</div>
         <div className="comment-time">12시간</div>
         <div><img className='comment-delete-button' src="./icon/comment-delete.svg" alt="" /></div>
-        <div className="comment-content">너무 멋있어</div>
-        <div className="comment-divider"></div>
+        <div className="comment-content">너무 멋있어서 미치겠네</div>
+        {/* <div className="comment-divider"></div> */}
     </div>
     );
 }
--- cookiewalk/src/main/comment.jsx
+++ cookiewalk/src/main/comment.jsx
@@ -19,7 +19,6 @@ export default function Comment(){
         <CommentContent />
         <CommentContent />
 
-
             <div className="comment-input-wrapper">
                 <img className="comment-input-avatar" src="./images/ellipse_7.png" alt="Avatar" />
                 <input
