62964884953dec94fbb7ab787e3fb5e7934813b4
Jistan7011 <qus7011@gmail.com>
2024-05-28 10:19:21 +0900

    댓글 삭제 접근제한, 댓글 작성 시간 삭제, css 수정 필요

--- cookiewalk/src/main/CommentContent.jsx
+++ cookiewalk/src/main/CommentContent.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './comment.css';
 
-export default function CommentContent({ commentID, userId, nickName, profileImage, comment, createdAt, deleteComment }) {
+export default function CommentContent({ commentID, userId, nickName, profileImage, comment, createdAt, deleteComment, userID }) {
     return (
         <div className="comment-wrapper">
             <img className="comment-avatar" src={profileImage} alt="Avatar" />
             <div className="comment-username">{nickName}</div>
-            <div className="comment-time">{new Date(createdAt).toLocaleString()}</div>
-            <div><img className='comment-delete-button' src="../../public/icon/comment-delete.svg" alt="Delete" onClick={() => deleteComment(commentID)} /></div>
+            <div className="comment-time">{new Date(createdAt).toLocaleDateString()}</div>
+            <div>
+                {userId === userID && (
+                     <img className='comment-delete-button' src="../../public/icon/comment-delete.svg" alt="Delete" onClick={() => deleteComment(commentID)} />
+                )}
+            </div> 
             <div className="comment-content">{comment}</div>
         </div>
     );
